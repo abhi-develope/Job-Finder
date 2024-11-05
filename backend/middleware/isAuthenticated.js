7c3f8ab140aa15493a999feccccce36e23fd37af
abhi-develope <prajapatiabhishek320@gmail.com>
2024-11-05 06:26:02 +0530

    company controller

--- backend/middleware/isAuthenticated.js
+++ backend/middleware/isAuthenticated.js
@@ -4,11 +4,11 @@ export const isAuthenticated = (req, res, next) => {
     const token = req.cookies.jobToken;
     if(!token) return res.status(401).json({error: "unauthorized - no token provided"})
         try {
-           const decoded = jwt.verify(token, process.env.JWT_SECRET);
+           const decoded = jwt.verify(token, process.env.JWT_SECRET);     
            
            if(!decoded) return res.status(401).json({error: "unauthorized - invalid token"})
 
-            req.userId = decoded.userId
+            req.id = decoded.userId
             next();
         } catch (error) {
             console.log("Error in verification", error);
