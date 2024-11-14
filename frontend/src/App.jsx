d99a6357fa2d925ed4fabaa91e054bbcafa2bdeb
abhi-develope <prajapatiabhishek320@gmail.com>
2024-11-14 18:18:47 +0530

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <p className='font-bold text-red-600 underline'>Lets build our application</p>
+    </div>
+  )
+}
+
+export default App
